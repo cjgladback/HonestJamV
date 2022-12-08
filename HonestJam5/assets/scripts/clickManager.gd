42063dc84a8ef22119362d9fb1d330f7f4b6eb90
cjgladback <cjgladback@gmail.com>
2022-12-08 14:08:32 -0500

    Can click a second box. Can't change anything about it yet.
    Attempting to use Dave the Dev's "How to Click 3D Objects in Godot | Game Dev Tutorial 8" but it's somewhat old and I want my script to be on a higher parent (to receive signals from all the possible boxes), which might be what's not working or it might just be I've set up my shader/material wrong to access its shader at all when what I want is to be able to access the color shader_param (but it's not acknowledged as existing for a CSGBox; might be an inherent issue with using primitives instead of instanced meshes).

--- HonestJam5/assets/scripts/clickManager.gd
+++ HonestJam5/assets/scripts/clickManager.gd
@@ -1,9 +1,7 @@
 extends Node
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var Number_Generator = RandomNumberGenerator.new()
 
 
 # Called when the node enters the scene tree for the first time.
@@ -14,3 +12,21 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_Area_input_event(camera, event, position, normal, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT and event.pressed == true:
+			print("Pressed Left Mouse Button")
+			self.randomize_color()
+			
+func randomize_color():
+	self.Number_Generator.randomize()
+	var new_red = self.Number_Generator.randf_range(0.0, 1.0)
+	self.Number_Generator.randomize()
+	var new_green = self.Number_Generator.randf_range(0.0, 1.0)
+	self.Number_Generator.randomize()
+	var new_blue = self.Number_Generator.randf_range(0.0, 1.0)
+	var new_color = Vector3(new_red, new_green, new_blue).normalized()
+#	var this_material = self.get_node("Drawers/CSGBox") # this is where I need to get the shader parameter "color" to affect
+#	this_material.albedo_color = Color(new_color.x, new_color.y, new_color.z, 1.0)
