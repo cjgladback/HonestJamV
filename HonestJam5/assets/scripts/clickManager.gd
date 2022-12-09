fd1049335d60000c4cd64ac1bcf3069aaaa12e42
cjgladback <cjgladback@gmail.com>
2022-12-09 16:14:06 -0500

    Have furniture (and frustration)
    Have the right kind of mesh instance (in that there is one instead of a primitive) to access its material and yet I still can't change its color.

--- HonestJam5/assets/scripts/clickManager.gd
+++ HonestJam5/assets/scripts/clickManager.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Spatial
 
 
 var Number_Generator = RandomNumberGenerator.new()
@@ -18,15 +18,15 @@ func _on_Area_input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and event.pressed == true:
 			print("Pressed Left Mouse Button")
-			self.randomize_color()
+			randomize_color()
 			
 func randomize_color():
-	self.Number_Generator.randomize()
-	var new_red = self.Number_Generator.randf_range(0.0, 1.0)
-	self.Number_Generator.randomize()
-	var new_green = self.Number_Generator.randf_range(0.0, 1.0)
-	self.Number_Generator.randomize()
-	var new_blue = self.Number_Generator.randf_range(0.0, 1.0)
+	Number_Generator.randomize()
+	var new_red = Number_Generator.randf_range(0.0, 1.0)
+	Number_Generator.randomize()
+	var new_green = Number_Generator.randf_range(0.0, 1.0)
+	Number_Generator.randomize()
+	var new_blue = Number_Generator.randf_range(0.0, 1.0)
 	var new_color = Vector3(new_red, new_green, new_blue).normalized()
-#	var this_material = self.get_node("Drawers/CSGBox") # this is where I need to get the shader parameter "color" to affect
-#	this_material.albedo_color = Color(new_color.x, new_color.y, new_color.z, 1.0)
+	var this_material = get_node("drawersBaseLoPoly/drawersBaseLoPoly").get_surface_material(0) # this is where I need to get the shader parameter "color" to affect
+	this_material.albedo_color = Color(new_color.x, new_color.y, new_color.z, 1.0)
